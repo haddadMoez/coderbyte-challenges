37daf382f961ab06231fab442e74e786d6977d41
HADDAD Moez <mhaddad@ats-digital.com>
2021-01-16 23:48:17 +0100

    First Reverse

---
+++ easy/first-reverse.js
@@ -0,0 +1,13 @@
+// Challenge: 
+// First Reverse
+// easy
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+// For example: if the input string is "Hello World and Coders" 
+// then your program should return the string sredoC dna dlroW olleH.
+
+// Proposed solution:
+function FirstReverse(str) { 
+    return str.split('').reverse().join('');   
+}
+    
+console.log(FirstReverse('Hello World and Coders'));
