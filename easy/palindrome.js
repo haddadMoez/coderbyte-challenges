4bf509f6e2c598231faf6212497a46b05f1fce1a
HADDAD Moez <mhaddad@ats-digital.com>
2021-01-17 11:56:09 +0100

    Palindrome

---
+++ easy/palindrome.js
@@ -0,0 +1,13 @@
+// Palindrome
+
+// Have the function Palindrome(str) take the str parameter being passed 
+// and return the string true if the parameter is a palindrome, 
+// (the string is the same forward as it is backward) otherwise return the string false.
+// For example: "racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
+
+// Proposed solution
+function Palindrome(str) { 
+    return str.split('').filter(item => item!=' ').join('').toLowerCase() == str.split('').reverse().filter(item=>item!=' ').join('').toLowerCase(); 
+}
+ 
+console.log(Palindrome('racecar'));
